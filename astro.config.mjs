0f090250b703820ca634ee338d2f279f6fe8dcb3
西田 圭悟 <keigo.nishida@sorich.jp>
2023-02-22 20:25:09 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: cloudflare()
+  output: "server",
+  adapter: cloudflare()
 });
